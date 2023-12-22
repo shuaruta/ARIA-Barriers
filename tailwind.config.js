7d2a5a92ec497ef9bbe0c161da1e81d53bef56f2
Takuya Nishimoto <nishimotz@gmail.com>
2023-12-22 13:50:17 +0900

    Add Rouge highlighter to default.html and update main.scss

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'rouge-bg': '#122b3b',
+      },
+    },
   },
   variants: {
     extend: {},
